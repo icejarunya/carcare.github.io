7ffb6bd94ca09a06d6322857d08c7bc083093b63
icejarunya <36377315+icejarunya@users.noreply.github.com>
2018-02-12 16:09:48 +0700

    หน้าแรกสมาชิก

---
+++ index.php
@@ -0,0 +1,105 @@
+<?PHP include"header.php";?>
+
+
+
+	
+
+<div class="container">
+<div align="center"><img src="images/bb.png" width="189" height="127" />
+</div>
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+     <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+
+
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+	​<br />
+      <div class="item active">
+        <img src="images/1111.jpg" alt="Los Angeles" style="width:100%;">
+      </div>
+​
+      <div class="item">
+         <img src="images/2222.jpg" alt="Los Angeles" style="width:100%;">
+      </div>
+    
+      <div class="item">
+         <img src="images/3333.jpg" alt="Los Angeles" style="width:100%;">
+      </div>
+</div>
+​
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+	 <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+ </a>
+</div>
+<!-- Page Content -->
+
+<div class="bs-example">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-4">
+                <a href="#" class="thumbnail">
+                    <img src="images/11.png" alt="125x125">
+                </a>
+            </div>
+            <div class="col-xs-4">
+                <a href="#" class="thumbnail">
+                   <img src="images/22.jpg" alt="125x125">
+                </a>
+            </div>
+            <div class="col-xs-4">
+                <a href="#" class="thumbnail">
+                     <img src="images/33.jpg" alt="125x125">
+                </a>
+            </div>
+			
+       
+		
+			
+			
+        </div>
+		
+		 <div class="row">
+            <div class="col-xs-4">
+                <a href="#" class="thumbnail">
+                    <img src="images/44.jpg" alt="125x125">
+                </a>
+            </div>
+            <div class="col-xs-4">
+                <a href="#" class="thumbnail">
+                   <img src="images/55.jpg" alt="125x125">
+                </a>
+            </div>
+            <div class="col-xs-4">
+                <a href="#" class="thumbnail">
+                     <img src="images/66.jpg" alt="125x125">
+                </a>
+            </div>
+		
+			
+			
+	
+			
+			
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+<?PHP include"footer.php";?>
